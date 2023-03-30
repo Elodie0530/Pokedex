6aa71ed07e3e370442e915f35d59328e401c318c
Elodie0530 <uneondine@gmail.com>
2023-03-30 11:50:45 +0200

    quete react 6 utilisation d'un state plus bonus

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import PokemonCard from "./components/PokemonCard"
-
+import { useState } from "react";
 
 function App() {
+  const [pokemonIndex, setpokemonIndex] = useState(0);
+
+  function next() {
+    setpokemonIndex(pokemonIndex+1)
+  }
+  function pre() {
+    setpokemonIndex(pokemonIndex-1)
+  }
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]}/>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      <p>Valeur du tableau: {pokemonIndex}</p>
+      <p>{pokemonIndex+1}/{pokemonList.length}</p>
+      {pokemonIndex > 0?<button onClick={pre}>Précédent</button>:<></>}
+      {pokemonIndex < pokemonList.length - 1?<button onClick={next}>Suivant</button>:<></>}
     </div>
-  );
+  )
+
 }
+
+
 
 const pokemonList = [
   {
@@ -16,10 +31,24 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
-
 
 
 export default App
